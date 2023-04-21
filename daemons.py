0c896066e5b1a182b87c79ca8ebd971a5a3c04d5
PJBoy <gcupcakekid@gmail.com>
2023-04-21 18:12:47 +0000

    Overhauled for discord.py update

--- daemons.py
+++ daemons.py
@@ -1,9 +1,10 @@
 import discord
-import aiohttp, feedparser
-import asyncio, datetime, html, random, re, time, urllib.parse
+import aiohttp, bs4, feedparser
+import asyncio, async_timeout, datetime, html, logging, random, re, time, traceback, urllib.parse
 
 
-metconst_update_channel_id = '518822602060464148'
+metconst_update_channel_id = 518822602060464148
+magconst_update_channel_id = 962419389720821760
 metconst_url_forum_profile = 'http://forum.metroidconstruction.com/'
 sleepTime = 10
 
@@ -12,27 +13,62 @@ def metconst_forum_profile_url(userId):
     return f'{metconst_url_forum_profile}?{queryString}'
 
 async def getFeed(url):
-    feed = feedparser.parse(url)
-    while not feed.entries:
+    while True:
+        try:
+            async with aiohttp.ClientSession() as session:
+                with async_timeout.timeout(sleepTime):
+                    async with session.get(url) as response:
+                        html = await response.text()
+                        feed = feedparser.parse(html)
+                        if feed.entries:
+                            break
+        except Exception as e:
+            logging.warning(f'{datetime.datetime.now()}:')
+            logging.warning(f'RSS feed parse error for {url}:')
+            logging.warning(traceback.format_exc())
+        
         await asyncio.sleep(sleepTime)
-        feed = feedparser.parse(url)
 
     return feed
 
 def toDateTime(structTime):
     return datetime.datetime.fromtimestamp(time.mktime(structTime))
 
+def remove_tags(text):
+    return bs4.BeautifulSoup(text, features = 'html.parser').get_text()
+
 def processHtml(text):
+    # print('Before:')
+    # print(text)
+    # print()
+    
+    # Hide spoilers
+    text = re.sub('<div.+?class="spoilerbody".*?></div>', r'', text)
+    text = re.sub('<div.+?class="spoilerbody".*?>(.*?)</div>', r'||\1||', text)
+    text = re.sub('<input.+?class="spoilerbutton".+?value="(.*?)".*?>(.*?)/>', r'\1 ', text)
+    
+    # Erase quotes
+    # text = re.sub('<div.+?class="topslice_quote".*?>.*?</div>', r'', text)
+    # text = re.sub('<div.+?class="quoteheader".*?>.*?</div>', r'', text)
+    # text = re.sub('<blockquote.+?class="bbc_standard_quote".*?>.*?</blockquote>', r'', text)
+    # text = re.sub('<div.+?class="botslice_quote".*?>.*?</div>', r'', text)
+    # text = re.sub('<div.+?class="quotefooter".*?>.*?</div>', r'', text)
+    
     text = text.replace(r'\n', '\n')
+    text = text.replace('<br />', '\n')
     text = text.replace('</div>', '\n')
     text = re.sub('<.+?>', '', text)
-    return html.unescape(text)
+    
+    # print('After:')
+    # print(html.unescape(text))
+    # print()
+    return html.unescape(text).replace('#039', '').replace('nbsp', '').replace('"quot', '"')
 
 async def processFeed(bot, rssUrl, entryProcessor):
     await bot.wait_until_ready()
     feed = await getFeed(rssUrl)
-    while not bot.is_closed:
-        mostRecentUpdated = feed.entries[0].updated_parsed
+    while not bot.is_closed():
+        mostRecentUpdated = max(entry.updated_parsed for entry in feed.entries)
         feed = await getFeed(rssUrl)
         for entry in reversed(feed.entries):
             if entry.updated_parsed <= mostRecentUpdated:
@@ -46,25 +82,27 @@ async def processFeed(bot, rssUrl, entryProcessor):
 async def metconst_forum(bot):
     'Does the usual forum updates'
 
-    metconst_url_forum_rss = 'http://forum.metroidconstruction.com/index.php?action=.xml;type=rss'
+    metconst_url_forum_rss = 'https://forum.metroidconstruction.com/index.php?action=.xml;type=rss&quotes=0'
 
     async def metconst_id(link):
-        async with aiohttp.get(link) as r:
-            if r.status != 200:
-                raise RuntimeError(f'{r.status} - {r.reason}')
+        async with aiohttp.ClientSession() as session:
+            async with session.get(link) as r:
+                if r.status != 200:
+                    raise RuntimeError(f'{r.status} - {r.reason}')
 
-            source = await r.text()
-            match = re.search(r'.*#msg(\d+)', link)
-            authorId, author = None, None
-            if match:
-                messageId = match[1]
-                match = re.search(f'id="msg{messageId}".+?<a href=".+?u=(\\d+)".+?>(.+?)</a>', source, flags = re.DOTALL)
+                source = await r.text()
+                match = re.search(r'.*#msg(\d+)', link)
+                authorId, author = None, None
                 if match:
-                    authorId, author = match[1], match[2]
+                    messageId = match[1]
+                    match = re.search(f'id="msg{messageId}".+?<a href=".+?u=(\\d+)".+?>(.+?)</a>', source, flags = re.DOTALL)
+                    if match:
+                        authorId, author = match[1], match[2]
 
-            return authorId, author
+                return authorId, author
 
     async def processEntry(entry):
+        logging.info(f'metconst_forum - {entry.title}')
         title = f'{entry.category} — {entry.title}'
         authorId, author = await metconst_id(entry.link)
 
@@ -75,64 +113,169 @@ async def metconst_forum(bot):
             else:
                 embed.set_author(name = author)
 
-        channel = discord.Object(id = metconst_update_channel_id)
-        await bot.send_message(channel, embed = embed)
+        await bot.get_channel(metconst_update_channel_id).send(embed = embed)
+        await bot.get_channel(magconst_update_channel_id).send(embed = embed)
 
     await processFeed(bot, metconst_url_forum_rss, processEntry)
 
 
-async def metconst_site(bot):
-    'Does the usual site updates'
+async def metconst_site_approved(bot):
+    'Does the site updates for inaccessible content'
 
-    metconst_url_files = 'http://ec2-18-218-195-111.us-east-2.compute.amazonaws.com:8001'
-    metconst_url_rss = 'http://metroidconstruction.com/recent.php?mode=atom&days=1&filters[]=New+Hack&filters[]=Hack+Updated&filters[]=Hack+Approved&filters[]=Hack+Review&filters[]=New+Resource&filters[]=Resource+Updated&filters[]=Resource+Approved&filters[]=Resource+Review&filters[]=Speedrun'
+    metconst_url_files = 'http://pjboy.cc:8001'
+    metconst_url_rss_approved = 'http://metroidconstruction.com/recent.php?mode=atom&days=1&filters[]=Hack+Approved&filters[]=Hack+Review&filters[]=Resource+Approved&filters[]=Resource+Review'
 
     async def metconst_id(hackLink, hackId):
-        async with aiohttp.get(hackLink) as r:
-            if r.status != 200:
-                raise RuntimeError(f'{r.status} - {r.reason}')
+        async with aiohttp.ClientSession() as session:
+            async with session.get(hackLink) as r:
+                if r.status != 200:
+                    raise RuntimeError(f'{r.status} - {r.reason}')
 
-            source = await r.text()
-            match = re.search(r'<b>Author:</b>\s*<a href=".+?action=profile;u=(\d+)">', source)
-            authorId = match[1] if match else None
-            screenshotUrls = re.findall(fr'<img src=".+?files/hacks/{hackId}/(.+?)".*?>', source)
-            return authorId, screenshotUrls
+                source = await r.text()
+                match = re.search(r'<b>Author:</b>\s*<a href=".+?action=profile;u=(\d+)">', source)
+                authorId = match[1] if match else None
+                
+                match = re.search(r'Pending</acronym>', source)
+                if match:
+                    stars_avg_score = None
+                    stars_avg = None
+                else:
+                    match = re.search(r'Average rating: (.+) chozo orbs', source)
+                    stars_avg_score = match[1] if match else None
+                
+                screenshotUrls = re.findall(fr'<img src=".+?files/(.+?/{hackId}/.+?)".*?>', source)
+                
+                matches = re.findall(r'(no_)?star\.png', source)
+                if not matches:
+                    stars = None
+                    stars_avg = None
+                else:
+                    stars = 0
+                    for i in range(5):
+                        if matches[-1 - i] != "no_":
+                            stars += 1
+                            
+                    if stars_avg_score != None:
+                        stars_avg = 0
+                        for i in range(5):
+                            if matches[i] != "no_":
+                                stars_avg += 1
+                    else:
+                        stars_avg = None
+                
+                return authorId, screenshotUrls, stars, stars_avg, stars_avg_score
 
     async def processEntry(entry):
-        embed = discord.Embed(title = entry.title, url = entry.link, timestamp = toDateTime(entry.updated_parsed))
+        logging.info(f'metconst_site - {entry.title}')
+        
+        if re.search(f'Resource Approved', entry.title):
+            embedColour = 0x00FF00
+        elif re.search(f'Resource Review', entry.title):
+            embedColour = 0x008000
+        elif re.search(f'Hack Approved', entry.title):
+            embedColour = 0x0000FF
+        elif re.search(f'Hack Review', entry.title):
+            embedColour = 0x000080
+        else:
+            embedColour = None
+        
+        embed = discord.Embed(title = entry.title, url = entry.link, timestamp = toDateTime(entry.updated_parsed), color = embedColour)
+        
         author = ', '.join(author.name for author in entry.authors)
         embed.set_author(name = author)
-        match = re.search(r'hack\.php\?hack_id=(\d+)', entry.link)
+            
+        match = re.search(r'(?:hack|resource)\.php\?(?:hack|resource)_id=(\d+)', entry.link)
+        stars = None
         if match:
             hackId = match[1]
-            authorId, screenshotUrls = await metconst_id(entry.link, hackId)
+            authorId, screenshotUrls, stars, stars_avg, stars_avg_score = await metconst_id(entry.link, hackId)
             if screenshotUrls:
                 screenshotUrl = random.sample(screenshotUrls, 1)[0]
-                screenshotUrl = f'{metconst_url_files}/metconst-hacks/{hackId}/{screenshotUrl}'
+                screenshotUrl = f'{metconst_url_files}/metconst-{screenshotUrl}'
                 embed.set_image(url = screenshotUrl)
 
             if authorId:
                 embed.set_author(name = author, url = metconst_forum_profile_url(authorId))
 
-        channel = discord.Object(id = metconst_update_channel_id)
-        await bot.send_message(channel, embed = embed)
+        msg = ''
+        if stars is not None:
+            msg += 'Latest: ' + ':full_moon: ' * stars + ':new_moon: ' * (5 - stars)
+        if stars_avg is not None:
+            msg += '\nAverage: ' + ':full_moon: ' * stars_avg + ':new_moon: ' * (5 - stars_avg) + f' ({stars_avg_score})'
+            
+        await bot.get_channel(metconst_update_channel_id).send(msg, embed = embed)
+        await bot.get_channel(magconst_update_channel_id).send(msg, embed = embed)
 
-    await processFeed(bot, metconst_url_rss, processEntry)
+    await processFeed(bot, metconst_url_rss_approved, processEntry)
+
+
+async def metconst_site_new(bot):
+    'Does the site updates for inaccessible content'
+
+    metconst_url_rss_new = 'http://metroidconstruction.com/recent.php?mode=atom&days=1&filters[]=New+Hack&filters[]=Hack+Updated&filters[]=New+Resource&filters[]=Resource+Updated&filters[]=Speedrun'
+
+    async def processEntry(entry):
+        logging.info(f'metconst_site - {entry.title}')
+        
+        if re.search(f'Resource Updated', entry.title):
+            embedColour = 0x004000
+        elif re.search(f'New Resource', entry.title):
+            embedColour = 0x004000
+        elif re.search(f'Hack Updated', entry.title):
+            embedColour = 0x000040
+        elif re.search(f'New Hack', entry.title):
+            embedColour = 0x000040
+        else:
+            embedColour = None
+        
+        embed = discord.Embed(title = entry.title, url = entry.link, timestamp = toDateTime(entry.updated_parsed), color = embedColour)
+        author = ', '.join(author.name for author in entry.authors)
+        embed.set_author(name = author)
+        msg = ''
+        
+        await bot.get_channel(metconst_update_channel_id).send(msg, embed = embed)
+        await bot.get_channel(magconst_update_channel_id).send(msg, embed = embed)
+
+    await processFeed(bot, metconst_url_rss_new, processEntry)
 
 
 async def metconst_wiki(bot):
     'Does the usual wiki updates'
 
-    metconst_url_wiki_rss = 'http://wiki.metroidconstruction.com/feed.php'
+    metconst_url_wiki_rss = 'https://wiki.metroidconstruction.com/feed.php'
 
     async def processEntry(entry):
+        logging.info(f'metconst_wiki - {entry.title}')
         embed = discord.Embed(title = entry.title, url = entry.link, description = processHtml(entry.summary), timestamp = toDateTime(entry.updated_parsed))
         author = ', '.join(author.name for author in entry.authors)
         embed.set_author(name = author)
-        channel = discord.Object(id = metconst_update_channel_id)
-        await bot.send_message(channel, embed = embed)
+        await bot.get_channel(metconst_update_channel_id).send(embed = embed)
+        await bot.get_channel(magconst_update_channel_id).send(embed = embed)
 
     await processFeed(bot, metconst_url_wiki_rss, processEntry)
 
 
-tasks = [metconst_forum, metconst_site, metconst_wiki]
+async def metconst_reddit(bot):
+    'Does the usual reddit updates'
+
+    metconst_url_reddit_rss = 'https://reddit.com/r/metroidconstruction.rss'
+
+    async def processEntry(entry):
+        logging.info(f'metconst_reddit - {entry.title}')
+
+        title = entry.title
+        url = entry.link
+        description = remove_tags(html.unescape(entry.content[0].value))
+        logging.info(description)
+        timestamp = toDateTime(entry.published_parsed)
+        embed = discord.Embed(title = title, url = url, description = description, timestamp = timestamp, color = 0x600000)
+        if entry.author is not None:
+            logging.info(f'{entry.author_detail!r}')
+            embed.set_author(name = entry.author, url = entry.author_detail.href)
+
+        await bot.get_channel(metconst_update_channel_id).send(embed = embed)
+
+    await processFeed(bot, metconst_url_reddit_rss, processEntry)
+
+
+tasks = [metconst_forum, metconst_site_approved, metconst_site_new, metconst_wiki, metconst_reddit]
---
+++ funny-man.py
@@ -0,0 +1,316 @@
+import daemons
+import discord
+import aiohttp, colour as color
+import argparse, bisect, json, logging, pathlib, random, re, string, traceback, urllib.parse
+from collections import namedtuple
+
+
+# Set up #
+logging.basicConfig(level = logging.INFO)
+
+def parseJsonFile(filepath):
+    with argparse.FileType()(filepath) as f:
+        return json.load(f)
+
+argparser = argparse.ArgumentParser(description = 'Run Hilarious Man, the Discord bot for you and me.')
+argparser.add_argument('config', type = parseJsonFile, nargs = '?', default = parseJsonFile(pathlib.PurePath(argparser.prog).with_suffix('.json')), help = 'Filepath to JSON file containing Hilarious Man configuration')
+args = argparser.parse_args()
+
+
+# Define bot #
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = discord.Client(intents = intents)
+
+@bot.event
+async def on_ready():
+    logging.info('Servers:')
+    for server in bot.guilds:
+        logging.info(f'{server} - {server.id!r}')
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    
+    if await trySimpleCommands(message):
+        return
+    
+    await tryCommands(message)
+
+def init():
+    def loadSimpleCommands():
+        global SimpleCommand, simpleCommands, commandRequests
+        
+        SimpleCommand = namedtuple('SimpleCommand', ['server', 'regex', 'message'])
+        simpleCommands = []
+        try:
+            with open('commands.json') as f:
+                simpleCommands = [SimpleCommand(*command) for command in json.load(f)]
+        except FileNotFoundError as e:
+            pass
+
+        commandRequests = []
+
+    def loadRamMap():
+        global ramMap, ramMapKeys
+        
+        ramMap = {}
+
+        with open('RAM map.asm', 'r') as f:
+            for line in f:
+                if 'VRAM (in word addresses, not byte addresses)' in line:
+                    break
+
+                match = re.match(r'\s*\$([0-7][0-9A-F]+)(?::([0-9A-F]+))?(:|\.\.).+', line, re.IGNORECASE)
+                if not match:
+                    continue
+
+                address = int(match[1], 16)
+                if match[2]:
+                    address = address << 16 | int(match[2], 16)
+                else:
+                    address |= 0x7E0000
+
+                ramMap[address] = match[0]
+
+        ramMapKeys = list(ramMap.keys())
+
+    def loadMiscFiles():
+        global pokemon, nicks
+        
+        with open('pokemon.json', 'r') as f:
+            pokemon = json.load(f)
+
+        with open('nicks.json', 'r') as f:
+            nicks = json.load(f)
+    
+    async def startDaemons():
+        for task in daemons.tasks:
+            bot.loop.create_task(task(bot))
+
+    loadSimpleCommands()
+    loadRamMap()
+    loadMiscFiles()
+    bot.setup_hook = startDaemons
+
+async def trySimpleCommands(message):
+    if message.guild is None:
+        return
+        
+    for command in simpleCommands:
+        if command.server != message.guild.id:
+            continue
+            
+        if command.regex.startswith('/'):
+            match = re.match(command.regex, message.content)
+        else:
+            match = re.search(command.regex, message.content)
+        if match:
+            await message.channel.send(command.message)
+            return True
+    
+    return False
+
+async def tryCommands(message):
+    for command in commands:
+        match = re.match(command.regex, message.content)
+        if match:
+            try:
+                await command.callback(message, match)
+                return
+            except Exception as e:
+                logging.warning(f'Command {command.regex!r} failed:')
+                logging.warning(traceback.format_exc())
+    
+Command = namedtuple('Command', ['regex', 'callback'])
+commands = []
+def makeCommand(regex):
+    def callback(callback):
+        global commands
+        
+        commands += [Command(regex, callback)]
+    
+    return callback
+
+
+# Bot commands #
+@makeCommand('/stadium')
+async def stadium(message, match):
+    'Picks three random Kanto pokemon and three random Johto pokemon'
+
+    await message.channel.send('\n'.join(f'{x}: {pokemon[x - 1]}' for x in sorted(random.sample(range(1, 149), 3) + random.sample(range(152, 248), 3))))
+
+@makeCommand(r'/aram2pc\s+(.+)')
+async def aram2pc(message, match):
+    'Calculates the file offset of an ARAM address'
+
+    translations = [
+        (0x1500, 0x56E2, 0x278104 + 4), # Main SPC engine
+        (0x5820, 0x5828, 0x27C2EA + 4), # Trackers
+        (0x6C00, 0x6C90, 0x278000 + 4), # ADSR settings
+        (0x6D00, 0x6D60, 0x27CF81 + 4), # Sample table
+        (0x6E00, 0xB516, 0x27D025 + 4)  # Sample data
+    ]
+
+    address = int(''.join(c for c in match[1] if c in string.hexdigits), 16)
+    if address < 0x1500:
+        await message.channel.send(f'${address:04X} is in SPC general purpose RAM, not loaded from ROM')
+        return
+
+    if address >= 0x10000:
+        await message.channel.send(f'${address:X} is outside the ARAM address space')
+        return
+
+    for (begin_aram, end_aram, begin_pc) in translations:
+        if begin_aram <= address < end_aram:
+            await message.channel.send(f'${address - begin_aram + begin_pc:06X}')
+            return
+    else:
+        await message.channel.send(f'${address:04X} is song-specific data, no unique ROM address')
+
+@makeCommand(r'/gb2pc\s+(.+)')
+async def gb2pc(message, match):
+    'Calculates the file offset of a game boy address'
+
+    address = int(''.join(c for c in match[1] if c in string.hexdigits), 16)
+    await message.channel.send(f'${address >> 2 & ~0x3FFF | address & 0x3FFF:05X}')
+
+@makeCommand(r'/pc2gb\s+(.+)')
+async def pc2gb(message, match):
+    'Calculates a game boy address from a file offset'
+
+    address = int(''.join(c for c in match[1] if c in string.hexdigits), 16)
+    if address < 0x4000:
+        await message.channel.send(f'${address:04X}')
+    else:
+        await message.channel.send(f'${address >> 14:X}:{address & 0x3FFF | 0x4000:04X}')
+
+@makeCommand(r'/snes2pc\s+(.+)')
+async def snes2pc(message, match):
+    'Calculates the file offset of a SNES address'
+
+    address = int(''.join(c for c in match[1] if c in string.hexdigits), 16)
+    await message.channel.send(f'${address >> 1 & 0x3F8000 | address & 0x7FFF:06X}')
+
+@makeCommand(r'/pc2snes\s+(.+)')
+async def pc2snes(message, match):
+    'Calculates a SNES address from a file offset'
+
+    address = int(''.join(c for c in match[1] if c in string.hexdigits), 16)
+    await message.channel.send(f'${address >> 15 | 0x80:02X}:{address & 0xFFFF | 0x8000:04X}')
+
+@makeCommand(r'/sm_?w?ram\s+(.+)')
+async def smRam(message, match):
+    'Looks up RAM address in Super Metroid RAM map'
+
+    address = int(''.join(c for c in match[1] if c in string.hexdigits), 16) | 0x7E0000
+    await message.channel.send(ramMap[ramMapKeys[bisect.bisect(ramMapKeys, address) - 1]])
+
+@makeCommand(r'/yt\s+(.+)')
+async def yt(message, match):
+    'Returns the first result for a youtube search'
+
+    youtube_url_results = 'http://www.youtube.com/results'
+    youtube_url_watch = 'http://www.youtube.com/watch'
+
+    async def yt_search(query):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(youtube_url_results, params = {'q': query}) as r:
+                if r.status != 200:
+                    await message.channel.send(f'YouTube error: `{r.status} - {r.reason}`')
+                    raise RuntimeError(f'{r.status} - {r.reason}')
+                    
+                source = await r.text()
+                return re.findall(r'"\/watch\?v=(.{11})', source)
+
+    def yt_watch_url(videoId):
+        queryString = urllib.parse.urlencode({'v': videoId})
+        return f'{youtube_url_watch}?{queryString}'
+
+    query = match[1]
+    results = await yt_search(query)
+    if len(results) == 0:
+        await message.channel.send('No results')
+        return
+
+    firstResult = results[0]
+
+    await message.channel.send(f'{yt_watch_url(firstResult)}')
+
+@makeCommand(r'/add_?command\s+"(.+?)"\s+"(.+?)"') # Need to address the fact that newline isn't supported for the response
+async def addSimpleCommand(message, match):
+    'Add a command. Format: /add "regex" "message"'
+    
+    regex, response = match[1], match[2]
+
+    # Check if valid regex
+    try:
+        re.compile(regex, re.IGNORECASE)
+    except re.error as e:
+        await message.channel.send(f'Invalid regex "{e.pattern}": {e.msg}')
+        return
+
+    # Add to queue
+    global commandRequests
+    commandRequests += [SimpleCommand(message.guild.id, regex, response)]
+    await message.channel.send(f'Your command has been requested, you are number {len(commandRequests)} in the queue')
+
+@makeCommand(r'/approve(?:\s+(\d+))?')
+async def approveSimpleCommand(message, match):
+    'Approves a command. Request queue is 1-indexed'
+    
+    global simpleCommands
+
+    if match[1]:
+        id = int(match[1])
+    else:
+        id = 1
+
+    # Assert privilege
+    if message.author.id != args.config['adminId']:
+        logging.info(f"{message.author.id = } != {args.config['adminId'] = }")
+        await message.channel.send('Denied')
+        return
+
+    # Assert valid id
+    if not 1 <= id <= len(commandRequests):
+        await message.channel.send('Invalid request id')
+        return
+
+    # Instatiate command and remove from requests queue
+    command = commandRequests.pop(id - 1)
+    simpleCommands += [command]
+
+    # Add to simple commands JSON file
+    with open('commands.json', 'w') as f:
+        json.dump(simpleCommands, f, indent = 4)
+
+    await message.channel.send(f'Approved command: {command.regex}')
+
+@makeCommand(r'/deny(?:\s+(\d+))?')
+async def denySimpleCommand(message, match):
+    'Denies a command. Request queue is 1-indexed'
+    
+    if match[1]:
+        id = int(match[1])
+    else:
+        id = 1
+
+    # Assert privilege
+    if message.author.id != args.config['adminId']:
+        await message.channel.send('Denied')
+        return
+
+    # Assert valid id
+    if not 1 <= id <= len(commandRequests):
+        await message.channel.send('Invalid request id')
+        return
+
+    command = commandRequests.pop(id - 1)
+    await message.channel.send(f'Denied command: {command.regex}')
+
+
+init()
+bot.run(args.config['token'])
